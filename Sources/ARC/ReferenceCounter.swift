231f95632f400e5df4334411fa759f239ca5a614
mazenalbaddad <mazen.albaddad@almosafer.com>
2026-01-04 21:03:07 +0300

    update reference counter algorithm

--- Sources/ARC/ReferenceCounter.swift
+++ Sources/ARC/ReferenceCounter.swift
@@ -9,30 +9,47 @@ import Foundation
 
 class ReferenceCounter {
     
+    private let identifierRegex = try! NSRegularExpression(
+        pattern: #"\b[A-Z][a-zA-Z0-9_]*\b"#,
+        options: []
+    )
+    
     func searchReferences(for objects: [Object]) -> [ObjectReference] {
-        var objectReferences: [ObjectReference] = []
-        for searchedObject in objects {
-            print("REFERENCE COUNTING \(searchedObject.name)")
-            let objectReference = ObjectReference(object: searchedObject)
-            objectReferences.append(objectReference)
-            if !searchedObject.systemObject {
-                for object in objects where searchedObject.name != object.name {
-                    for codeBlock in object.codeBlocks {
-                        if isLine(codeBlock.concatenatedLines, contains: searchedObject.name) {
-                            objectReference.add(reference: object)
-                            break
-                        }
+        let nonSystemObjects = objects.filter { !$0.systemObject }
+        let objectNameSet = Set(nonSystemObjects.map { $0.name })
+        var objectReferenceMap: [String: ObjectReference] = [:]
+        for object in objects {
+            objectReferenceMap[object.name] = ObjectReference(object: object)
+        }
+        
+        for object in objects {
+            print("REFERENCE COUNTING \(object.name)")
+            let referencedNames = extractReferencedNames(from: object, knownNames: objectNameSet)
+            for referencedName in referencedNames where referencedName != object.name {
+                if let referencedObjectRef = objectReferenceMap[referencedName] {
+                    referencedObjectRef.add(reference: object)
+                }
+            }
+        }
+        return objects.compactMap { objectReferenceMap[$0.name] }
+    }
+    
+    private func extractReferencedNames(from object: Object, knownNames: Set<String>) -> Set<String> {
+        var foundNames: Set<String> = []
+        for codeBlock in object.codeBlocks {
+            let text = codeBlock.concatenatedLines
+            let range = NSRange(text.startIndex..., in: text)
+            let matches = identifierRegex.matches(in: text, options: [], range: range)
+            
+            for match in matches {
+                if let matchRange = Range(match.range, in: text) {
+                    let identifier = String(text[matchRange])
+                    if knownNames.contains(identifier) {
+                        foundNames.insert(identifier)
                     }
                 }
             }
         }
-        return objectReferences
-    }
-    
-    private func isLine(_ line: String, contains name: String) -> Bool {
-        let regexPattern = #"(?<!\p{L})(\#(name))(?!\p{L})"#
-        let regex = try! NSRegularExpression(pattern: regexPattern, options: [])
-        let matches = regex.matches(in: line, options: [], range: NSRange(line.startIndex..., in: line))
-        return matches.count > 0
+        return foundNames
     }
 }
